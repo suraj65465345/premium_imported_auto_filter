2199e96685a41acbf9fbec71dcbbaf052270296c
suraj65465345 <suraj65465345@gmail.com>
2024-10-01 02:20:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ STREAMHTO = (environ.get('STREAMHTO', ''))
 BOT_USERNAME = environ.get("BOT_USERNAME", "sks1749bot")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6507579310').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6507579310 5277442733').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001738336615').split()] #Channel id for auto indexing ( make sure bot is admin )
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6507579310').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
